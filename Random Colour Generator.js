a90f1b74c4db6a957f74affad2ac7231bf8690ca
stevens5 <94493468+stevens5@users.noreply.github.com>
2022-02-05 21:20:24 +0100

    Random color page

---
+++ Random Colour Generator.js
@@ -0,0 +1,31 @@
+ const containerElement = document.querySelector(".container")
+
+ for (let index =  0; index < 30; index++){
+     const colorContainerElement = document.createElement("div")
+     colorContainerElement.classList.add("color-container")
+     containerElement.appendChild(colorContainerElement);
+ } 
+
+const colorContainerElements = document.querySelectorAll(".color-container");
+
+// console.log(colorContainerElements)
+generateColors();
+
+function generateColors() {
+    colorContainerElements.forEach((colorContainerElement) => {
+        const newColorCode = randomColor();
+        colorContainerElement.style.backgroundColor = "#" + newColorCode;
+        colorContainerElement.innerText = "#" + newColorCode;
+    });
+} 
+
+ function randomColor(){
+     const chars = "0123456789abcdef"
+     const colorCodeLength = 6;
+     let colorCode = ""
+     for (let index = 0; index < colorCodeLength; index++){
+         const randomNum  = Math.floor(Math.random() * chars.length) 
+         colorCode += chars.substring(randomNum, randomNum + 1);
+     }
+     return colorCode; 
+ }
